2a0a5294f86c75c9afe90b4d2627d0bf965cac3e
Stallon Pinto <stallonpinto03@gmail.com>
2022-11-18 19:22:33 -0800

    Completed the GUI of my code

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -6,12 +6,6 @@ import java.io.FileNotFoundException;
 // initializes the ticketing system
 public class Main {
     public static void main(String[] args) {
-        try {
-            new Gui();
-            new TicketingSystem();
-        } catch (FileNotFoundException e) {
-            System.out.println("Cant run application- no file found.");
-
-        }
+        new Gui();
     }
 }
